cddd223daa5fd60776da553923091d3e7ea1470e
Jack Painter <littlesympathy@jacks-mbp.home>
2020-08-11 15:03:55 +0100

    Add useEffect to Timer to decrement seconds state

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import PlayerSelection from "./PlayerSelection"
 import Timer from "./Timer"
 import { StyledBody, StyledMain } from "./styled/lib"
 
 const App = () => {
+  const [seconds, setSeconds] = useState(15)
+
   return (
     <StyledBody>
       <StyledMain>
-        <Timer />
+        <Timer seconds={seconds} setSeconds={setSeconds} />
         <PlayerSelection />
       </StyledMain>
     </StyledBody>
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,10 +1,18 @@
-import React from "react"
+import React, { useEffect } from "react"
 
 import { StyledTimer } from "./styled/lib";
 
-const Timer = () => {
+const Timer = ({ seconds, setSeconds }) => {
 
-    return <StyledTimer></StyledTimer>
+    useEffect(() => {
+        if(seconds > 0) {
+            setTimeout(() => {
+                setSeconds(seconds => seconds - 1)
+            }, 1000)
+        }
+    }, [seconds, setSeconds]) 
+
+    return <StyledTimer>{seconds}</StyledTimer>
 }
 
 export default Timer 
