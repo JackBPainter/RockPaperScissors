d3fc8277e2ae6309558c61f327cea90a9b99f830
Jack Painter <littlesympathy@jacks-mbp.home>
2020-08-11 11:38:25 +0100

    Create basic layout for game

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { StyledBody, StyledMain } from "../styled/lib"
+
+function App() {
+  return (
+    <StyledBody>
+      <StyledMain>
+      </StyledMain>
+    </StyledBody>
+  );
+}
+
+export default App;
---
+++ src/styled/lib.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const StyledBody = styled.body`
+  display: flex;
+  justify-content: center;
+  background: #083846;
+`
+
+export const StyledMain = styled.main`
+  width: 80vw;
+  height: 100vh;
+  background: #fff; 
+`
