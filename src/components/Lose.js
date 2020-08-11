bfa97a6a5825d4741544e2f6c4e56511c714792c
Jack Painter <littlesympathy@jacks-mbp.home>
2020-08-11 17:56:06 +0100

    Show the bot and player's selections in Lose

--- src/components/Lose.js
+++ src/components/Lose.js
@@ -1,11 +1,18 @@
-import React from "react"
+import React from "react";
+import iconSelect from "../functions/iconSelect";
 
-import { StyledMain } from "./styled/lib"
+import { StyledMain, StyledOptionContainer, StyledOption } from "./styled/lib";
 
-const Lose = () => {
-    return (
-        <StyledMain result={"lose"}>Game Over!</StyledMain>
-    )
-}
+const Lose = ({ playerSelection, botSelection }) => {
+  return (
+    <StyledMain result={"lose"}>
+      <StyledOptionContainer>
+        <StyledOption>{iconSelect(playerSelection)}</StyledOption>
+        VS
+        <StyledOption>{iconSelect(botSelection)}</StyledOption>
+      </StyledOptionContainer>
+    </StyledMain>
+  );
+};
 
-export default Lose
+export default Lose;
