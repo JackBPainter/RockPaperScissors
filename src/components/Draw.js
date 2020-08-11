be445959eba24666ae9c82e8858c29cafb477cd4
Jack Painter <littlesympathy@jacks-mbp.home>
2020-08-11 17:55:23 +0100

    Show the bot and player's selections in Draw

--- src/components/Draw.js
+++ src/components/Draw.js
@@ -1,11 +1,18 @@
-import React from "react"
+import React from "react";
+import iconSelect from "../functions/iconSelect";
 
-import { StyledMain } from "./styled/lib"
+import { StyledMain, StyledOptionContainer, StyledOption } from "./styled/lib";
 
-const Draw = () => {
-    return (
-        <StyledMain result={"draw"}>Draw!</StyledMain>
-    )
-}
+const Draw = ({ playerSelection, botSelection }) => {
+  return (
+    <StyledMain result={"draw"}>
+      <StyledOptionContainer>
+        <StyledOption>{iconSelect(playerSelection)}</StyledOption>
+        VS
+        <StyledOption>{iconSelect(botSelection)}</StyledOption>
+      </StyledOptionContainer>
+    </StyledMain>
+  );
+};
 
-export default Draw
+export default Draw;
