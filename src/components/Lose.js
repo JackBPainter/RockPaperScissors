6f14278e39a3882f78a68b7cbabbd5134271f68c
Jack Painter <littlesympathy@jacks-mbp.home>
2020-08-11 18:45:48 +0100

    Add conditional statement to Lose component

--- src/components/Lose.js
+++ src/components/Lose.js
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import iconSelect from "../functions/iconSelect";
 
 import { StyledMain, StyledOptionContainer, StyledOption } from "./styled/lib";
 
 const Lose = ({ playerSelection, botSelection }) => {
+  const [count, setCount] = useState(3);
+
+  useEffect(() => {
+    if (count > 0) {
+      setTimeout(() => {
+        setCount((count) => count - 1);
+      }, 1000);
+    }
+  }, [count, setCount]);
+
   return (
-    <StyledMain result={"lose"}>
-      <StyledOptionContainer>
-        <StyledOption>{iconSelect(playerSelection)}</StyledOption>
-        VS
-        <StyledOption>{iconSelect(botSelection)}</StyledOption>
-      </StyledOptionContainer>
+    <StyledMain result={count === 0 ? "lose" : "neutral"}>
+      {count === 0 ? (
+        "Game Over!"
+      ) : (
+        <StyledOptionContainer>
+          <StyledOption>{iconSelect(playerSelection)}</StyledOption>
+          VS
+          <StyledOption>{iconSelect(botSelection)}</StyledOption>
+        </StyledOptionContainer>
+      )}
     </StyledMain>
   );
 };
